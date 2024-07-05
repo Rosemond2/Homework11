1d35cf701021586ee4452b94f444de3df1e678fd
Rosemond2 <rosemondgyi04@gmail.com>
2024-07-05 14:47:52 +0000

    adding changes in scr folder

--- Src/change.js
+++ Src/change.js
@@ -1,19 +1,19 @@
 
-//   let first = document.getElementById("topic");
-//   first.style.backgroundColor= "purple";
-//   first.innerHTML= "you are welcome";
+   let first = document.getElementById("topic");
+   first.style.backgroundColor= "purple";
+   first.innerHTML= "you are welcome";
 
-//   let second = document.querySelector("#heading");
-//   second.style.color= "yellow";
+   let second = document.querySelector("#heading");
+   second.style.color= "yellow";
 
-//   let third = document.getElementById("tab");
-//   third.style.padding= "50px"
-//   third.style.width= "50%";
+   let third = document.getElementById("tab");
+   third.style.padding= "50px"
+   third.style.width= "50%";
 
-//   let forth = document.getElementById("last");
-//   forth.style.color= "purple";
-//   forth.innerHTML= "See you again soon, bye👋";
-//   forth.style.textShadow= "2px 2px 2px white";
+   let forth = document.getElementById("last");
+   forth.style.color= "purple";
+   forth.innerHTML= "See you again soon, bye👋";
+  forth.style.textShadow= "2px 2px 2px white";
 
 function findMe(){
     let pfour = document.getElementById("pfour");

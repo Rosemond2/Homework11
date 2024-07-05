7a34c429ed25e7d7f997599a5faf9f8de7c5d33e
Rosemond2 <rosemondgyi04@gmail.com>
2024-07-05 10:10:13 +0000

    adding src folder

---
+++ Src/change.js
@@ -0,0 +1,16 @@
+
+let first = document.getElementById("topic");
+first.style.backgroundColor= "purple";
+first.innerHTML= "you are welcome";
+
+let second = document.querySelector("#heading");
+second.style.color= "yellow";
+
+let third = document.getElementById("tab");
+third.style.padding= "50px";
+third.style.width= "50%";
+
+let forth = document.getElementById("last");
+forth.style.color= "purple";
+forth.innerHTML= "See you again soon, bye👋";
+forth.style.textShadow= "2px 2px 2px white";
